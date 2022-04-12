4556443243d72b71fc9c1e4351586c7bb350170b
jennyhedley <jennyhedley@yahoo.com>
2022-04-12 14:19:52 +1000

    Set up

---
+++ bot.js
@@ -0,0 +1,28 @@
+console.log("The bot is starting");
+
+require("dotenv").config();
+const Twit = require("twit");
+const config = require("./config");
+
+const T = new Twit(config);
+
+const getTweet = require("./tweets");
+
+tweetIt();
+setInterval(tweetIt, 1000 * 7200);
+
+function tweetIt() {
+  const tweet = {
+    status: getTweet(),
+  };
+
+  T.post("statuses/update", tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("It worked");
+    }
+  }
+}
