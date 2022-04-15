c3876b0ee3dc70a6460bb0e054126ed8d4941c3f
jennyhedley <jennyhedley@yahoo.com>
2022-04-15 20:47:25 +1000

    cron schedule

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
-console.log("The bot is starting");
+console.log("Anne bot is starting");
 
 require("dotenv").config();
+const cron = require("node-cron");
 const Twit = require("twit");
 const config = require("./config");
-
 const T = new Twit(config);
-
 const getTweet = require("./tweets");
 
-tweetIt();
-setInterval(tweetIt, 1000 * 60 * 60 * 6);
+// Tweet every day at 5am 10am and 5pm
+cron.schedule("0 5,10,17 * * *", () => {
+  tweetIt();
+});
+
+// tweetIt();
+// setInterval(tweetIt, 1000 * 60 * 60 * 6);
 
 function tweetIt() {
   const tweet = {
