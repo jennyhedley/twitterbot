c75e9a08784609c183f490c6664d8645d56938fd
jennyhedley <jennyhedley@yahoo.com>
2022-04-15 22:08:05 +1000

    add express and port config and Procfile

--- bot.js
+++ bot.js
@@ -1,5 +1,23 @@
 console.log("Anne bot is starting");
 
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+
+//For avoiding Heroku $PORT error
+app
+  .get("/", function (request, response) {
+    const result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
+
 require("dotenv").config();
 const cron = require("node-cron");
 const Twit = require("twit");
